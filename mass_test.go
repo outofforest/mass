a7907c7a9b9b0feeadecb6fec0f65303df136d86
Wojciech Malota-Wojcik <wojtek@exw.co>
2024-09-04 06:16:37 +0200

    Implement slice allocation

--- mass_test.go
+++ mass_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestMass(t *testing.T) {
+func TestNew(t *testing.T) {
 	m := New[int](10)
 	os := make([]*int, 0, 100)
 	for i := range cap(os) {
@@ -17,4 +17,24 @@ func TestMass(t *testing.T) {
 	for i, o := range os {
 		require.Equal(t, i, *o)
 	}
+}
+
+func TestNewSlice(t *testing.T) {
+	m := New[int](10)
+	os := [][]int{}
+	expected := [][]int{}
+
+	k := 0
+	for i := range 50 {
+		o := m.NewSlice(i)
+		e := make([]int, 0, len(o))
+		for j := range o {
+			o[j] = k
+			e = append(e, k)
+			k++
+		}
+		os = append(os, o)
+		expected = append(expected, e)
+	}
+	require.Equal(t, expected, os)
 }
